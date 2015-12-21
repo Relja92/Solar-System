77e7d488d14a53edeb7af34c70d3b96ccb9fa0ce
Marko Reljic <marko.reljic@hinttech.com>
2015-12-21 17:33:48 +0100

    Dymanicly generate planets

---
+++ js/planets.js
@@ -0,0 +1,12 @@
+
+    var planetStats =[
+        {"name":"mercury", "size":1,distance:30,speed:0.01},
+        {"name":"venus", "size":2,distance:56,speed:0.00825},
+        {"name":"earth", "size":2,distance:78,speed:0.007125},
+        {"name":"mars", "size":1,distance:99,speed:0.005985},
+        {"name":"jupiter", "size":6,distance:114,speed:0.005055},
+        {"name":"saturn", "size":5,distance:139,speed:0.00425},
+        {"name":"uranus", "size":3,distance:156,speed:0.003525},
+        {"name":"neptune", "size":3,distance:178,speed:0.003},
+        {"name":"pluto", "size":1,distance:201,speed:0.002}
+    ]
